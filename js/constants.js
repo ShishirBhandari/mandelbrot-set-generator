8a6c98d68dda84d6b5ee48697dbb8e20c7fb249b
ShishirBhandari <shishirbhandari54@gmail.com>
2020-07-13 22:31:28 +0545

    Setup constants for the app

---
+++ js/constants.js
@@ -0,0 +1,10 @@
+// Constants
+
+var ITERS = 100.0;
+var LIMIT = 2.0;
+var SCALE = 1.0;
+var OFFSET = [0.0, 0.0];
+var C_REAL = 0.0;
+var C_IMAG = 0.0;
+var TINT_COLOR = [1.0, 1.0, 1.0, 1.0];
+var COLOR_FREQ = 0.0;
