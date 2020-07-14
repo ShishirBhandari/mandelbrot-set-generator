6681ae419d55243550d3c72fec90aaf1a119f5bf
ShishirBhandari <shishirbhandari54@gmail.com>
2020-07-14 20:30:24 +0545

    Combine two shaders into one and add fractal selection uniform

--- js/shaders.js
+++ js/shaders.js
@@ -14,64 +14,13 @@ void main() {
 }
 `;
 
-// Fragment shader
-var juliaSetFragmentShaderText = `
-precision mediump float;
-#define iterations x
-uniform float time;
 
-uniform vec4 color;
-uniform float color_frequency;
-
-uniform float iters;
-uniform float scale;
-uniform vec2 offset;
-uniform float limit;
-uniform float c_real;
-uniform float c_imag;
-
-varying vec4 _pos;
-
-float map(float value, float min1, float max1, float min2, float max2) {
-  return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
-}
-
-void main() {
-  float scale = 1.0 / scale;
-
-  float real = (_pos[0]) * scale  + offset.x;
-  float img = (_pos[1]) * scale + offset.y;
-  
-  float fractalColor = 0.0;
-  for(float i=0.0; i<5000.0; i++) {
-    if(i >= iters) break;
-
-    float real_prev = real;
-    real = real_prev * real_prev - img * img + c_real;
-    img = 2.0 * real_prev * img + c_imag;
-
-    if(real * real + img * img > limit) {
-        fractalColor = i / iters;
-    }
-  }
-
-  float r = map(fractalColor * fractalColor, 0.0, 0.3 * 0.3, 0.0, 1.0);
-  float g = map(fractalColor, 0.3, 0.6, 0.0, 1.0);
-  float b = map(sqrt(fractalColor), sqrt(0.6), 1.0, 0.0, 1.0);
-
-  vec3 julia_color = vec3(r, g, b);
-  vec4 final_color = vec4(julia_color, 1.0) * color;
-  final_color.a = final_color.a * (sin(color_frequency*time) / 2.0 + 1.0);
-  gl_FragColor = final_color;
-}
-`;
-
-
-
-var mandelbrotSetFragmentShaderText = `
+// Fractal Fragment shader
+var fractalFragmentShaderText = `
 precision highp float;
 #define iterations x
 uniform float time;
+uniform bool is_mandelbrot;
 
 uniform vec4 color;
 uniform float color_frequency;
@@ -89,12 +38,7 @@ float map(float value, float min1, float max1, float min2, float max2) {
   return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
 }
 
-void main() {
-  float scale = 1.0 / scale;
-
-  float real = (_pos[0]) * scale  + offset.x;
-  float img = (_pos[1]) * scale + offset.y;
-
+float get_mandelbrot_set(float real, float img, float c_real, float c_imag, float iters) {
   float cr = real;
   float ci = img;
 
@@ -114,6 +58,36 @@ void main() {
     }
   }
 
+  return fractalColor;
+}
+
+float get_julia_set(float real, float img, float c_real, float c_imag, float iters) {
+  float fractalColor = 0.0;
+  for(float i=0.0; i<5000.0; i++) {
+    if(i >= iters) break;
+
+    float real_prev = real;
+    real = real_prev * real_prev - img * img + c_real;
+    img = 2.0 * real_prev * img + c_imag;
+
+    if(real * real + img * img > limit) {
+        fractalColor = i / iters;
+    }
+  }
+
+  return fractalColor;
+}
+
+void main() {
+  float scale = 1.0 / scale;
+
+  float real = (_pos[0]) * scale  + offset.x;
+  float img = (_pos[1]) * scale + offset.y;
+
+  float fractalColor = 0.0;
+  if (is_mandelbrot) fractalColor = get_mandelbrot_set(real, img, c_real, c_imag, iters);
+  else fractalColor = get_julia_set(real, img, c_real, c_imag, iters);
+
   float r = map(fractalColor * fractalColor, 0.0, 0.3 * 0.3, 0.0, 1.0);
   float g = map(fractalColor, 0.3, 0.6, 0.0, 1.0);
   float b = map(sqrt(fractalColor), sqrt(0.6), 1.0, 0.0, 1.0);
@@ -124,6 +98,4 @@ void main() {
   gl_FragColor = final_color;
 }
 `;
-
-
 
